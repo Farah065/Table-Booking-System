74d2c062798c875c7fc1cb53cbd7d9765cabee9c
Farah065 <farahahmad065@gmail.com>
2024-02-22 17:52:29 +0200

    added about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,3 +1,6 @@
+import Image1 from '../Images/mario-adrian-1.jpg';
+import Image2 from '../Images/mario-adrian-2.jpg';
+
 function About() {
     return (
         <article className="about">
@@ -13,6 +16,10 @@ function About() {
                     <br /> <br />
                     Join us for a journey through the Mediterranean, where each bite tells a story, and every moment is a celebration of flavor and connection.
                 </p>
+            </div>
+            <div className="images">
+                <img src={Image1} alt="adrian and mario chatting with bowls of food in front of them" width={216} height={232} id="img1" />
+                <img src={Image2} alt="adrian and mario laughing together in the kitchen" width={216} height={232} id="img2" />
             </div>
         </article>
     );
