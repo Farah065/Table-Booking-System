191aa4c763a18b6b6cd2a872eb3295c45c037c27
Farah065 <farahahmad065@gmail.com>
2024-02-22 20:12:57 +0200

    added footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,53 @@
+import { Link } from 'react-router-dom';
+import Logo from '../Images/logo-large.png';
+
 function Footer() {
+    function scrollToTop() {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    
     return (
         <footer>
-
+            <div className="footer-contents">
+                <img src={Logo} alt="a large version of the little lemon logo" width={96} />
+                <section>
+                    <header>
+                        <h3>Navigation</h3>
+                    </header>
+                    <nav className="footer-nav">
+                        <Link to="/" className="nav-item" onClick={scrollToTop}>Home</Link>
+                        <Link to="/about" className="nav-item" onClick={scrollToTop}>About</Link>
+                        <Link to="/menu" className="nav-item" onClick={scrollToTop}>Menu</Link>
+                        <Link to="/reservations" className="nav-item" onClick={scrollToTop}>Reservations</Link>
+                        <Link to="/order-online" className="nav-item" onClick={scrollToTop}>Order Online</Link>
+                        <Link to="/login" className="nav-item" onClick={scrollToTop}>Login</Link>
+                    </nav>
+                </section>
+                <section>
+                    <header>
+                        <h3>Contact</h3>
+                    </header>
+                    <ul>
+                        <li>123 Olive Street, Citrus Grove, Mediterra City, FL 33123</li>
+                        <li>(555) 123-4567</li>
+                        <li>info@little-lemon.com</li>
+                    </ul>
+                </section>
+                <section>
+                    <header>
+                        <h3>Social Media</h3>
+                    </header>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                    </ul>
+                </section>
+            </div>
         </footer>
     );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,17 +22,25 @@ function Navbar() {
           window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
+    function scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
     
     return (
         <div className="nav-container" ref={navbar}>
             <LogoSVG />
             <nav className="nav">
-                <Link to="/" className="nav-item">HOME</Link>
-                <Link to="/about" className="nav-item">ABOUT</Link>
-                <Link to="/menu" className="nav-item">MENU</Link>
-                <Link to="/reservations" className="nav-item">RESERVATIONS</Link>
-                <Link to="/order-online" className="nav-item">ORDER ONLINE</Link>
-                <Link to="/login" className="nav-item">LOGIN</Link>
+                <Link to="/" className="nav-item" onClick={scrollToTop}>HOME</Link>
+                <Link to="/about" className="nav-item" onClick={scrollToTop}>ABOUT</Link>
+                <Link to="/menu" className="nav-item" onClick={scrollToTop}>MENU</Link>
+                <Link to="/reservations" className="nav-item" onClick={scrollToTop}>RESERVATIONS</Link>
+                <Link to="/order-online" className="nav-item" onClick={scrollToTop}>ORDER ONLINE</Link>
+                <Link to="/login" className="nav-item" onClick={scrollToTop}>LOGIN</Link>
             </nav>
         </div>
     );
