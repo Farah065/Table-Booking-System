97b308b3f3013261bb423362eaae3ff663a03dc2
Farah065 <farahahmad065@gmail.com>
2024-02-25 21:05:48 +0200

    finished reservation page header section

--- src/Components/ResHeader.js
+++ src/Components/ResHeader.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {ReactComponent as CheckmarkSVG} from '../SVGs/checkmark.svg'
 
 function ResHeader() {
     const [step, setStep] = useState(1);
@@ -8,24 +9,32 @@ function ResHeader() {
     }
 
     return (
+        <>
         <header className="res-header-container">
             <div className="res-header">
                 <h1>Reserve a Table</h1>
                 <div className="steps">
                     <div id={step === 1 ? "on" : "complete"}>
-                        {step <= 1 ? <h2>1</h2> : <></>}
+                        {step <= 1 ? <h2>1</h2> : <CheckmarkSVG />}
                     </div>
                     <div id={step === 2 ? "on" : step > 2 ? "complete" : ""}>
-                        {step <= 2 ? <h2>2</h2> : <></>}
+                        {step <= 2 ? <h2>2</h2> : <CheckmarkSVG />}
                     </div>
                     <div id={step === 3 ? "on" : step > 2 ? "complete" : ""}>
-                        {step <= 3 ? <h2>3</h2> : <></>}
+                        {step <= 3 ? <h2>3</h2> : <CheckmarkSVG />}
                     </div>
-                    <hr className={step === 1 ? "step-1" : step === 2 ? "step-2" : "step-3"} />
+                    <hr className="hr-bg" />
+                    <hr className={step === 1 ? "step-1 hr-fill" : step === 2 ? "step-2 hr-fill" : "step-3 hr-fill"} />
                 </div>
-                <button className="primary-btn" onClick={handleClick}>next</button>
+                <div className="step-names">
+                    <p>Resrvation<br />Details</p>
+                    <p>Personal<br />Info</p>
+                    <p>Confirmation</p>
+                </div>
             </div>
         </header>
+        <button className="primary-btn" onClick={handleClick}>next</button>
+        </>
     );
 }
 
